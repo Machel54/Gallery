696d75fd66dfe974f20efae230f869a515fd1899
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-11 07:33:42 +0300

    create Editor test and test is functional

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Editor,tags,Category,Location
 
 # Create your tests here.
+# Create your tests here.
+class EditorTestClass(TestCase):
+    def setUp(self):
+        self.killa=  Editor(first_name = 'machel', last_name ='killa', email ='killa@gmail.com')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.killa,Editor))
+        
+    def test_save_method(self):
+        self.killa.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+        
