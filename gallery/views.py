835991afe730006301cc07bcd1785d285f232cbd
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-11 13:04:54 +0300

    Make major update to the program

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
+from .models import Image
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html')
+    new = Image.get_image()
+    return render(request, 'index.html', {"new":new})
+
+def search_results(request):
+    
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render (request, 'search.html', {"message":message, "images":searched_images})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, 'index.html', {"image": image})
