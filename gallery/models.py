fb3a201e9543fe9b9299cd0f352e24e09c7832c1
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-11 06:19:59 +0300

    Create Editor, tag model and initialize them in the db

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+    
+    def __str__(self):
+        return self.first_name
+    
+    def save_editor(self):
+        self.save()
+    
+    class Meta:
+        ordering = ['first_name']
+        verbose_name = 'Editor'
+        verbose_name_plural = 'Editors'
+    
+class tags(models.Model):
+    name = models.CharField(max_length= 20)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
